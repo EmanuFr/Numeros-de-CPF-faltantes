059058e071ea5cfc22a142c093bbf3b27e59cb78
EmanuFr <emanufr27@gmail.com>
2025-10-13 19:30:04 -0300

    Regiao adicionada ao script.js

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ let numeroQueSatisfaz;
 let numeroFaltante = CPF.slice(0,9).indexOf(null)
 let numeroFaltanteVerificador = CPF.slice(9,11).indexOf(null) + 9
 let numeroVerificadorPresente;
+let regiao;
 
 if (numeroFaltanteVerificador == 9){
     numeroVerificadorPresente = 10
@@ -48,6 +49,19 @@ for(numeroQueSatisfaz = 0; numeroQueSatisfaz <= 9; numeroQueSatisfaz = numeroQue
     } 
     
 }
+switch(CPF[8]){
+    case 0 : regiao = 'Rio Grande do Sul'; break;
+    case 1 : regiao = 'Distrito Federal, Goiás, Mato Grosso, Mato Grosso do Sul e Tocantins'; break;
+    case 2 : regiao = 'Amazonas, Pará, Roraima, Amapá, Acre e Rondônia'; break;
+    case 3 : regiao = 'Ceará, Maranhão e Piauí'; break;
+    case 4 : regiao = 'Paraíba, Pernambuco, Alagoas e Rio Grande do Norte'; break;
+    case 5 : regiao = 'Bahia e Sergipe'; break;
+    case 6 : regiao = 'Minas Gerais'; break;
+    case 7 : regiao = 'Rio de Janeiro e Espírito Santo'; break;
+    case 8 : regiao = 'São Paulo'; break;
+    case 9 : regiao = 'Paraná e Santa Catarina'; break;
+    default: regiao = 'Número inválido'; 
+}
 
-console.log(`O primeiro digito verificador é ${CPF[9]} e o segundo dígito verificador é ${CPF[10]}. O CPF completo é ${CPF.slice(0, 3).join('')}.${CPF.slice(3,6).join('')}.${CPF.slice(6,9).join('')}-${CPF.slice(9,11).join('')}.`);
+console.log(`O primeiro digito verificador é ${CPF[9]} e o segundo dígito verificador é ${CPF[10]}. O CPF completo é ${CPF.slice(0, 3).join('')}.${CPF.slice(3,6).join('')}.${CPF.slice(6,9).join('')}-${CPF.slice(9,11).join('')} localizado na regiao de ${regiao}.`);
 
